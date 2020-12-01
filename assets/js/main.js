62453a113c12d3db7e3b1fff602c686be53205f7
Ubuntu <ubuntu@ip-172-31-36-103.eu-west-1.compute.internal>
2020-12-01 23:53:30 +0000

    User comments added for imrpoved UI.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,3 @@ Array.from(document.querySelectorAll(".nav-button")).forEach((item) => {
         item.parentElement.parentElement.classList.toggle("change");
     };
 });
-
-// Website author developer years
-
-$(document).ready(function () {
-
-    // Website authors developer years appear in the footer
-     
-    $('#developer-years').text('2018 - ' + new Date().getFullYear());
-    
-});
