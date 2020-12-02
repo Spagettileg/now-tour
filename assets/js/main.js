4188eaac8f1b14b34ecd611e8c04782f7fd6dde8
Ubuntu <ubuntu@ip-172-31-36-103.eu-west-1.compute.internal>
2020-12-02 03:19:38 +0000

    Array of background colours function made available for user clicking on navbar item. Change function created triggered by user click to open and close navbar view.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+// Change
+
+const container = document.querySelector('.container');
+
+document.querySelector('.open-navbar-icon').addEventListener("click", () => {
+    container.classList.add('change');
+});
+
+document.querySelector('.close-navbar-icon').addEventListener("click", () => {
+    container.classList.remove('change');
+});
+
+
+// Array of colours
+
+const colors = ["#6495ED", "#7FFFD4", "#FFA07A", "#F08080", "#AFEEEE"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
+    item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
 // Toggle function to enable tour cards to flip from top to rear.
 
 Array.from(document.querySelectorAll(".nav-button")).forEach((item) => {
@@ -5,3 +28,4 @@ Array.from(document.querySelectorAll(".nav-button")).forEach((item) => {
         item.parentElement.parentElement.classList.toggle("change");
     };
 });
+
