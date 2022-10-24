e17616125a614ab6fcc32e8f17ac40afc1dd0547
EC2 Default User <paul.b.friel@me.com>
2022-10-24 16:24:52 +0000

    JQuery code snippet added to support developer years

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,12 @@ Array.from(document.querySelectorAll(".nav-button")).forEach((item) => {
     };
 });
 
+// Copyright Year
+
+$(document).ready(function () {
+
+    // Website authors developer years appear in the footer
+     
+    $('#developer-years').text('2018 - ' + new Date().getFullYear());
+    
+});
